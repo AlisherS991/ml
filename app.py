0876200a4604a873b5f99a29119af1ceae4c9af7
AlisherS991 <msmanash971@gmail.com>
2026-05-06 00:06:14 +0500

    commit fixed 2nd time

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import model
+import os # Add this at the top of your file
 
 app = Flask(__name__)
 
@@ -21,4 +22,7 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Railway provides the port via an environment variable
+    port = int(os.environ.get("PORT", 5000))
+    # '0.0.0.0' tells Flask to listen on all available network interfaces
+    app.run(host='0.0.0.0', port=port)
